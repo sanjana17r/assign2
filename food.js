6f9bcabdf8eef49f4776757ef8c1312ede4da80d
sanjana17r <38381244+sanjana17r@users.noreply.github.com>
2018-05-24 12:11:55 +0530

    Add files via upload

--- food.js
+++ food.js
@@ -1,16 +1,16 @@
-var items=["burger","pizza","pasta"];//these are the array of items ordered and served
-var table=[1,2,3];//array of table numbers to which they are to be served.
 
+var items=["burger","pizza","pasta"];//these are the array of items ordered and served
+var table=100;// table number to which they are to be served.
+
+//here let 'a' represent items and 'n' represent table number.
 function preparefood(a,n,callback)//function for preparing food
 {
-    console.log("preparing food for items: " + items)//prints the items
-    console.log("table: " + table)//prints the table numbers
-    callback(a,n)//calls servefood function within this fuction.
+    console.log("Preparing food for items: " + items)//prints the items
+    console.log("Serving food for items: " + callback(a,n) )
 }
 function servefood(a,n)//function for serving food
 {
-console.log("serving food for items: " + items)//prints the items
-console.log("table: " + table)//prints the table numbers
+return items;
 }
 
 
